58f73864acb2f2caf9bccb95036a63b02dfd29c7
Christophe Jollivet <jollivet@apside.fr>
2023-03-08 14:12:42 +0100

    observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { map, Observable, Subscription, take } from 'rxjs';
+import { DemoObservableService } from '../common/demo-observable.service';
 
 @Component({
   selector: 'crm-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnDestroy{
+
+  myObservable?:Observable<number>;
+  private subscriptions: Subscription[] = [];
+
+  constructor(private demoObservable: DemoObservableService){}
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(s=>s.unsubscribe());
+  }
+  callObservable():void{
+    const subscription = this.demoObservable.test().pipe(
+          map(x=>x*10),
+          take(4)
+        ).subscribe({
+        next: (value:number)=>{ console.log(value)},
+        error: (error:Error)=>{ console.error(error)},
+        complete: ()=>{console.log('complete')}
+      }
+    );
+    this.subscriptions.push(subscription);
+  }
+  callWithAsync():void{
+    this.myObservable = this.demoObservable.test()
+  }
 
 }
